52f2a8fdf2a7eae013b60a28e9d1ccd774f44003
Nico <nico@aleph.engineering>
2020-01-01 12:30:03 -0500

    Update Dashboard view

--- backend/views/site/dashboard.php
+++ backend/views/site/dashboard.php
@@ -2,77 +2,33 @@
 /* @var $this yii\web\View */
 
 $this->registerJs('demo.initDashboardPageCharts();');
-    $this->title = 'Administración';
+$this->title = 'Administración';
+$salesInfo = $data['sales'];
+$currSalesInfo = $salesInfo['currentInfo'];
 ?>
 
 <div class="content">
     <div class="container-fluid">
         <div class="row">
-            <div class="col-lg-3 col-md-6 col-sm-6">
-                <div class="card card-stats">
-                    <div class="card-header" data-background-color="orange">
-                        <i class="material-icons">content_copy</i>
-                    </div>
-                    <div class="card-content">
-                        <p class="category">Used Space</p>
-                        <h3 class="title">49/50<small>GB</small></h3>
-                    </div>
-                    <div class="card-footer">
-                        <div class="stats">
-                            <i class="material-icons text-danger">warning</i> <a href="#pablo">Get More Space...</a>
+            
+            <?php foreach ($currSalesInfo as $currSaleInfo) { ?>
+                <div class="col-lg-3 col-md-6 col-sm-6">
+                    <div class="card card-stats">
+                        <div class="card-header" data-background-color="green">
+                            <i class="material-icons">store</i>
+                        </div>
+                        <div class="card-content">
+                            <p class="category"><i class="material-icons">card_giftcard</i> VENTAS</p>
+                        </div>
+                        <div class="card-footer" style="margin-top: 30px;">
+                            <p class="category"><i class="material-icons">date_range</i> <?= $currSaleInfo['title'] ?></p>
+                            <h5 class="title">Ingresos: $<?= $currSaleInfo['data'] ? $currSaleInfo['data']->amount : "0" ?></h5>
+                            <h5 class="title">Ganancia: $<?= $currSaleInfo['data'] ? $currSaleInfo['data']->profit : "0" ?></h5>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-lg-3 col-md-6 col-sm-6">
-                <div class="card card-stats">
-                    <div class="card-header" data-background-color="green">
-                        <i class="material-icons">store</i>
-                    </div>
-                    <div class="card-content">
-                        <p class="category">Revenue</p>
-                        <h3 class="title">$34,245</h3>
-                    </div>
-                    <div class="card-footer">
-                        <div class="stats">
-                            <i class="material-icons">date_range</i> Last 24 Hours
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6 col-sm-6">
-                <div class="card card-stats">
-                    <div class="card-header" data-background-color="red">
-                        <i class="material-icons">info_outline</i>
-                    </div>
-                    <div class="card-content">
-                        <p class="category">Fixed Issues</p>
-                        <h3 class="title">75</h3>
-                    </div>
-                    <div class="card-footer">
-                        <div class="stats">
-                            <i class="material-icons">local_offer</i> Tracked from Github
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-lg-3 col-md-6 col-sm-6">
-                <div class="card card-stats">
-                    <div class="card-header" data-background-color="blue">
-                        <i class="fa fa-twitter"></i>
-                    </div>
-                    <div class="card-content">
-                        <p class="category">Followers</p>
-                        <h3 class="title">+245</h3>
-                    </div>
-                    <div class="card-footer">
-                        <div class="stats">
-                            <i class="material-icons">update</i> Just Updated
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <?php } ?>
+            
         </div>
 
         <div class="row">
@@ -82,13 +38,7 @@ $this->registerJs('demo.initDashboardPageCharts();');
                         <div class="ct-chart" id="dailySalesChart"></div>
                     </div>
                     <div class="card-content">
-                        <h4 class="title">Daily Sales</h4>
-                        <p class="category"><span class="text-success"><i class="fa fa-long-arrow-up"></i> 55%  </span> increase in today sales.</p>
-                    </div>
-                    <div class="card-footer">
-                        <div class="stats">
-                            <i class="material-icons">access_time</i> updated 4 minutes ago
-                        </div>
+                        <p class="category">&Uacute;ltimos d&iacute;as</p>
                     </div>
                 </div>
             </div>
@@ -99,15 +49,8 @@ $this->registerJs('demo.initDashboardPageCharts();');
                         <div class="ct-chart" id="emailsSubscriptionChart"></div>
                     </div>
                     <div class="card-content">
-                        <h4 class="title">Email Subscriptions</h4>
-                        <p class="category">Last Campaign Performance</p>
+                        <p class="category">&Uacute;ltimas semanas</p>
                     </div>
-                    <div class="card-footer">
-                        <div class="stats">
-                            <i class="material-icons">access_time</i> campaign sent 2 days ago
-                        </div>
-                    </div>
-
                 </div>
             </div>
 
@@ -117,13 +60,7 @@ $this->registerJs('demo.initDashboardPageCharts();');
                         <div class="ct-chart" id="completedTasksChart"></div>
                     </div>
                     <div class="card-content">
-                        <h4 class="title">Completed Tasks</h4>
-                        <p class="category">Last Campaign Performance</p>
-                    </div>
-                    <div class="card-footer">
-                        <div class="stats">
-                            <i class="material-icons">access_time</i> campaign sent 2 days ago
-                        </div>
+                        <p class="category">&Uacute;ltimos meses</p>
                     </div>
                 </div>
             </div>
